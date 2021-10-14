96a9a426b4ba0b536d67844ac77a1bc6698e988b
ytang5 <ytang5@trip.com>
2021-10-14 16:53:16 +0800

    fix: 修改跨域域名

--- pages/api/getNickName.js
+++ pages/api/getNickName.js
@@ -3,7 +3,7 @@ import Cors from 'cors'
 
 const cors = Cors({
     methods: ['GET', 'HEAD'],
-    origin: ['http://localhost:3000', 'http://sugarfish:3000']
+    origin: ['http://localhost:3000', 'http://www.sugarfish.top:3000']
 })
 
 const nickname = new RandomNickname()
