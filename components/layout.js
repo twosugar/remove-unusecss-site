9ae0a4e01bbdb145056025939d072b5ed71a8f74
ytang5 <ytang5@trip.com>
2021-11-01 17:54:29 +0800

    feat: 更新keyword

--- components/layout.js
+++ components/layout.js
@@ -2,23 +2,23 @@
  * @Description: 
  * @Date: 2021-08-19 15:38:03
  * @FilePath: /remove-unusecss-site/components/layout.js
- * @LastEditTime: 2021-11-01 16:40:02
+ * @LastEditTime: 2021-11-01 17:53:48
  */
 import Head from 'next/head'
 import styles from './layout.module.css'
 
-export const siteTitle = 'js随机生成昵称'
+export const siteTitle = '移除不必要的css，提取关键css'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>随机生成昵称</title>
+        <title>移除不必要的css，提取关键css</title>
         <link rel="icon" href="/img/cola.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, viewport-fit=cover, minimal-ui"/>
         <meta name="description" content={siteTitle}/>
         <meta name="og:title" content={siteTitle} />
-        <meta name="keywords" content="web前端,拉罐,随机生成昵称,js" />
+        <meta name="keywords" content="web前端,拉罐,移除不必要的css,提取关键css,puppeteer" />
       </Head>
       <main>{children}</main>
     </div>
