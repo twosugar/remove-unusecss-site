b7cc5c28346087c015010541e2e6383f6cb83a76
ytang5 <ytang5@trip.com>
2021-11-01 17:28:51 +0800

    fix: 接口parse情况兼容

--- pages/api/getUnuseCss.js
+++ pages/api/getUnuseCss.js
@@ -1,8 +1,8 @@
 import RemoveUnusecss from '../../lib/removeActionConfig/index'
 
 export default async (req, res) => {
-    const params = JSON.parse(req.body) ?? {}
+    const params = req.body ? JSON.parse(req.body) : {}
     console.log('params.config', params.config)
-    const data = await RemoveUnusecss.start(params.config)
+    const data = await RemoveUnusecss.start(params.config || {})
     res.status(200).json({ data: data, success: true })
 }
